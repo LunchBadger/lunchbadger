b54820096c0fbc211c7c2c09adf99038961a366e
Roman Lisagor <rlisagor@gmail.com>
2016-07-20 13:37:30 -0700

    Models have relations

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var morgan = require('morgan');
 
 var app = module.exports = loopback();
+
+app.use(morgan('dev'))
 
 app.start = function() {
   // start the web server
