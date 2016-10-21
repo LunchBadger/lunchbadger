a815ce2081b92d45c831c34a40ebb2eb94d0f685
Roman Lisagor <rlisagor@gmail.com>
2016-10-21 09:42:12 -0700

    CORS in workspace instance

--- server/boot/workspace.js
+++ server/boot/workspace.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const path = require('path');
+const cors = require('cors');
 
 process.env.WORKSPACE_DIR = path.normalize(path.join(__dirname, '../../workspace'));
 const workspace = require('loopback-workspace');
+
+workspace.middleware('initial', cors({
+  origin: true,
+  credentials: true,
+  maxAge: 86400,
+  exposedHeaders: ["ETag"]
+}));
 
 module.exports = function(app) {
   workspace.listen(app.get('workspacePort'), app.get('host'), function() {
