4982497876179bdb8dccdfdaa6f84e95ef527123
Roman Lisagor <rlisagor@gmail.com>
2016-11-18 09:32:47 -0800

    Ensure the workspace is initialized whenever the server starts

--- server/boot/workspace.js
+++ server/boot/workspace.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 
@@ -13,10 +14,28 @@ workspace.middleware('initial', cors({
   exposedHeaders: ["ETag"]
 }));
 
-module.exports = function(app) {
+module.exports = function(app, cb) {
   app.workspace = workspace;
   workspace.listen(app.get('workspacePort'), app.get('host'), function() {
-    console.log(`Managing workspace in ${process.env.WORKSPACE_DIR}`);
     console.log(`Workspace listening at http://${app.get('host')}:${app.get('workspacePort')}`);
   });
+
+  ensureWorkspace(workspace, cb);
 };
+
+function ensureWorkspace(workspaceApp, cb) {
+  let Workspace = workspaceApp.models.Workspace;
+
+  let userName = process.env.LB_USER || 'workspace';
+  let userEnv = process.env.LB_ENV || 'dev';
+  let wsName = `${userName}-${userEnv}`;
+
+  let pkgFile = path.join(process.env.WORKSPACE_DIR, 'package.json');
+  if (!fs.existsSync(pkgFile)) {
+    console.log(`Creating workspace in ${process.env.WORKSPACE_DIR}`);
+    Workspace.createFromTemplate('empty-server', wsName, cb);
+  } else {
+    console.log(`Managing workspace in ${process.env.WORKSPACE_DIR}`);
+    cb();
+  }
+}
