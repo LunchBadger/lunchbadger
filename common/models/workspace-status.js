65167054188d16e16ae59a0e9d46e85e22587aae
Roman Lisagor <rlisagor@gmail.com>
2016-11-24 14:49:07 -0800

    Ping method to help UI figure out when workspace is ready

--- common/models/workspace-status.js
+++ common/models/workspace-status.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(WorkspaceStatus) {
+  WorkspaceStatus.ping = function(cb) {
+    cb();
+  }
 
+  WorkspaceStatus.remoteMethod('ping', {
+    description: "For checking whether the server is up. Responds with 204.",
+    http: { verb: 'get', path: '/ping' }
+  });
 };
