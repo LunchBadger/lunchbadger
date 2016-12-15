8225540c5ccc78174e5f0402a614f1eed17ea087
Roman Lisagor <rlisagor@gmail.com>
2016-12-14 17:00:42 -0800

    Commit to Git after project save

--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,7 @@
 const async = require('async');
 const debug = require('debug')('lunchbadger-workspace:project');
+const promisify = require('es6-promisify');
+const exec = promisify(require('child_process').exec);
 
 module.exports = function(Project) {
 
@@ -36,6 +38,12 @@ module.exports = function(Project) {
     });
   };
 
+  Project.remoteMethod('clearProject', {
+    description: 'Clear all data from the project, including the workspace',
+    isStatic: false,
+    http: { path: '/clear', verb: 'post' }
+  });
+
   Project.prototype._deleteAllModels = function(cb) {
     let wsModels = Project.app.workspace.models;
 
@@ -53,11 +61,35 @@ module.exports = function(Project) {
         async.series(tasks, callback);
       }
     ], cb);
-  }
+  };
 
-  Project.remoteMethod('clearProject', {
-    description: 'Clear all data from the project, including the workspace',
-    isStatic: false,
-    http: { path: '/clear', verb: 'post' }
-  })
+  Project.observe('after save', function(ctx, next) {
+    exec('git status', {cwd: process.env.WORKSPACE_DIR})
+      .then((stdout) => {
+        if (!stdout.includes('nothing to commit')) {
+          debug('changes detected, committing');
+          return commit();
+        } else {
+          debug('nothing to commit');
+        }
+      }).then(() => {
+        next(null);
+      }).catch(err => {
+        console.log(err.stack);
+        next(new Error('Error saving project'));
+      });
+  });
 };
+
+function commit() {
+  return exec('git add -A', {cwd: process.env.WORKSPACE_DIR})
+    .then(() => {
+      return exec('git commit -m "Changes via LunchBadger"',
+                  {cwd: process.env.WORKSPACE_DIR});
+    })
+    .then(() => {
+      return exec('git push', {cwd: process.env.WORKSPACE_DIR})
+        .catch((err) => {
+        });
+    })
+}
