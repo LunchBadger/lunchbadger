fd2610cb61f17ac3dad5cbdde33deed0759124e4
kolbinski <olbik80@gmail.com>
2017-09-19 14:25:51 +0200

    Update config.js renaming branch into master

--- common/lib/config.js
+++ common/lib/config.js
@@ -7,7 +7,7 @@ const userEnv = process.env.LB_ENV || 'dev';
 module.exports = {
   userEnv: userEnv,
   //branch: `env/${userEnv}`,
-  branch: 'features/service-and-api-endpoints',
+  branch: 'master',
   userName: process.env.LB_USER || 'workspace',
   gitUrl: process.env.GIT_URL || 'http://localhost:3002/git/demo.git',
   urlPrefix: process.env.WORKSPACE_URL_PREFIX,
