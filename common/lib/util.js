f7706d118b9c91eb1cabaa69f6d7a3ed392eb8c7
Serhii Kuts <sergeykuc@gmail.com>
2018-11-26 14:44:47 +0200

    repositioning rebase

--- common/lib/util.js
+++ common/lib/util.js
@@ -57,10 +57,11 @@ async function push (branch) {
     // Note async version usage: 
     // sync version will block process for time to push (5 sec) and readyness probe will fail
     lock = true;
-    await execWsAsync(`git pull --rebase -s recursive -X ours origin ${branch}`);
-    // git pull --rebase -s recursive -X ours origin master
+    await execWsAsync(`git pull origin ${branch} --rebase -s recursive -X ours`);
+    // git pull origin master --rebase -s recursive -X ours 
     // will never have merge conflicts 
     // in case of conflict it always chooses remote changes (-X ours flag)
+    // it seems to be important to have --rebase after branch name
     await execWsAsync(`git push origin ${branch} --porcelain`);
     debug('pushing');
     lock = false;
